f4b2f6afd15a79cbb7eed32fdeba02ca2b8674d0
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-07 10:30:24 +0530

    Added: Validation of Last Name

--- src/com/bridgelabz/regex/ValidationOfUserRegistration.java
+++ src/com/bridgelabz/regex/ValidationOfUserRegistration.java
@@ -3,11 +3,21 @@ package com.bridgelabz.regex;
 import java.util.Scanner;
 
 public class ValidationOfUserRegistration {
-	Scanner scanner = new Scanner(System.in);
-	public void validateFirstName(){
-		System.out.println("Enter the First Name");
-		String firstName = scanner.next();
-		String regex = "^[A-Z][a-z]{2,}$";
-		System.out.println(firstName.matches(regex));
-	}
+	
+    Scanner scanner = new Scanner(System.in);
+    
+    public void validateFirstName(){
+        System.out.println("Enter the First Name");
+        String firstName = scanner.next();
+        String regex = "^[A-Z][a-z]{2,}$";
+        System.out.println(firstName.matches(regex));
+    }
+
+    public void validateLastName(){
+        System.out.println("Enter the Last Name");
+        String lastName = scanner.next();
+        String regex = "^[A-Z][a-z]{2,}$";
+        System.out.println(lastName.matches(regex));
+
+    }
 }
