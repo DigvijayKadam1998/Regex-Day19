96d89259001127746c9b8acd4446378fc73e5be9
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-07 10:40:14 +0530

    Added: Validation of Mobile Number

--- src/com/bridgelabz/regex/ValidationOfUserRegistration.java
+++ src/com/bridgelabz/regex/ValidationOfUserRegistration.java
@@ -4,25 +4,24 @@ import java.util.Scanner;
 
 public class ValidationOfUserRegistration {
     Scanner scanner = new Scanner(System.in);
-    
-    public void validateFirstName(){
-        System.out.println("Enter the First Name");
-        String firstName = scanner.next();
+   
+    public void validateFirstName(String firstName){
         String regex = "^[A-Z][a-z]{2,}$";
         System.out.println(firstName.matches(regex));
     }
-    
-    public void validateLastName(){
-        System.out.println("Enter the Last Name");
-        String lastName = scanner.next();
+
+    public void validateLastName(String lastName){
         String regex = "^[A-Z][a-z]{2,}$";
         System.out.println(lastName.matches(regex));
     }
 
-    public void validateEmailId(){
-        System.out.println("Enter the EmailId");
-        String emailId = scanner.next();
+    public void validateEmailId(String emailId){
         String regex = "^[A-Za-z0-9-._]{1,}+@+[a-z]{1,}+.+[a-z]$";
         System.out.println(emailId.matches(regex));
+    }
+
+    public void validateMobileNumber(String mobileNumber){
+        String regex = "^[0-9]{2}+ +[0-9]{10}$";
+        System.out.println(mobileNumber.matches(regex));
     }
 }
