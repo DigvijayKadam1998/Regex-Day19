6c6ba6426ddd0613e8ab7166a95de5225a2e0a72
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-07 10:23:07 +0530

    Welcome Message

---
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.regex;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
+		validationOf.validateFirstName();
+	}
+}
